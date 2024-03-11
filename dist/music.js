d0edae58224e19cf5a8d4ba7cd556ebed2ca735b
Ozone <2039992322@qq.com>
2024-03-11 17:08:27 +0800

    Site updated: 2024-03-11 17:08:27

--- dist/music.js
+++ dist/music.js
@@ -12,13 +12,13 @@ const ap = new APlayer({
         {
             name: '蜘蛛糸モノポリ',
             artist: 'sasakure.UK / 初音ミク',
-            url: 'https://www.ytmp3.cn/down/58072.mp3',
+            url: 'http://music.163.com/song/media/outer/url?id=26440351.mp3',
             cover: 'http://p1.music.126.net/b6cwIaAUy5MXSm3iNC0KNg==/109951163352158677.jpg?param=130y130',
         },
         {
             name: '丸の内サディステック',
             artist: '椎名林檎',
-            url: 'https://m701.music.126.net/20240310153921/aeb8b9e7d6a39ea25a0b4d9f240ec90c/jdyyaac/520f/0e5e/005a/ee51973fe4944c7afee070edfac6d4dc.m4a',
+            url: 'http://music.163.com/song/media/outer/url?id=1381350481.mp3',
             cover: 'http://p1.music.126.net/W4JdzEv6rWH-9axhEUGW3w==/719080604577566.jpg?param=130y130',
         }
     ]
