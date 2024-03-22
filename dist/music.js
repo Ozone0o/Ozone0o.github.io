4b9fe44a4cdea74853ff0de5054c8c5cff5860f5
Ozone <2039992322@qq.com>
2024-03-22 17:51:38 +0800

    Site updated: 2024-03-22 17:51:38

--- dist/music.js
+++ dist/music.js
@@ -21,5 +21,23 @@ const ap = new APlayer({
             url: 'http://music.163.com/song/media/outer/url?id=1381350481.mp3',
             cover: 'http://p1.music.126.net/W4JdzEv6rWH-9axhEUGW3w==/719080604577566.jpg?param=130y130',
         }
+        {
+            name: 'Value',
+            artist: 'Ado',
+            url: 'http://music.163.com/song/media/outer/url?id=2129117330.mp3',
+            cover: 'http://p1.music.126.net/7gzYj1itC6BpKDlk6sZtTA==/109951169359997375.jpg?param=130y130',
+        }
+        {
+            name: 'Sorrowful',
+            artist: 'WHIZZ',
+            url: 'http://music.163.com/song/media/outer/url?id=1861779056.mp3',
+            cover: 'http://p1.music.126.net/EElO6OYMbcAVJOzcyuB-rA==/109951166185185693.jpg?param=130y130',
+        }
+        {
+            name: '波兰首都是上海',
+            artist: 'yourboyfriendsucks!',
+            url: 'http://music.163.com/song/media/outer/url?id=419596181.mp3',
+            cover: 'http://p2.music.126.net/S0NDCxJ7__2DuC3q8rHm_A==/109951164419258439.jpg?param=130y130',
+        }
     ]
 });
